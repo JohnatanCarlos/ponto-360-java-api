03c835ec4d74d91502094c28dc08c5c32c57d381
jcosta.wayon <jcosta.wayon@veloe.com.br>
2025-11-06 19:30:33 -0300

    feat: habilitar todos para chamar os endpoints

--- src/main/java/com/ponto_360/core/common/config/WebConfig.java
+++ src/main/java/com/ponto_360/core/common/config/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry)  {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
